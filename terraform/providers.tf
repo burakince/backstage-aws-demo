6e2de7898a5fb79218791f088bf89fdf732cd6ea
Burak Ince <burak.ince@linux.org.tr>
2023-01-30 04:42:28 +0100

    Add assume role to provider.
    Signed-off-by: Burak Ince <burak.ince@linux.org.tr>

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -10,4 +10,9 @@ terraform {
 # Configure the AWS Provider to Frankfurt
 provider "aws" {
   region = local.region
+
+  assume_role {
+    role_arn     = "arn:aws:iam::775254019113:role/github-role"
+    session_name = "githubrolesession"
+  }
 }
