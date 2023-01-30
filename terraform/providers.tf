879a99a201673191f17c9afeaddb1d09ecc0abcd
Burak Ince <burak.ince@linux.org.tr>
2023-01-30 06:43:59 +0100

    Change assume role structure.
    Signed-off-by: Burak Ince <burak.ince@linux.org.tr>

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -10,13 +10,13 @@ terraform {
 provider "aws" {
   region = local.region
 
-  assume_role {
-    role_arn = "arn:aws:iam::775254019113:role/github-role"
-  }
-
-  # assume_role_with_web_identity {
-  #   role_arn                = "arn:aws:iam::775254019113:role/github-role"
-  #   session_name            = "githubrolesession"
-  #   web_identity_token_file = var.web_identity_token_file
+  # assume_role {
+  #   role_arn = "arn:aws:iam::775254019113:role/github-role"
   # }
+
+  assume_role_with_web_identity {
+    role_arn = "arn:aws:iam::775254019113:role/github-role"
+    #   session_name            = "githubrolesession"
+    #   web_identity_token_file = var.web_identity_token_file
+  }
 }
