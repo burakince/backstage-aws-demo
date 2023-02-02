51eba4ddc58c744c36d38904b4f5d2c73d65bd68
Burak Ince <burak.ince@linux.org.tr>
2023-02-02 08:11:54 +0100

    Add override for logging setting.
    Signed-off-by: Burak Ince <burak.ince@linux.org.tr>

--- terraform/main.tf
+++ terraform/main.tf
@@ -38,8 +38,15 @@ resource "aws_cloudwatch_log_group" "backstage" {
 resource "aws_ecs_cluster" "ecs" {
   name = "${local.name}-ecs"
 
+  setting {
+    name  = "containerInsights"
+    value = "enabled"
+  }
+
   configuration {
     execute_command_configuration {
+      logging = "OVERRIDE"
+
       log_configuration {
         cloud_watch_log_group_name = aws_cloudwatch_log_group.backstage.name
       }
